cb7993380ccf20cd2875375fb1a2d6d0a2dd5872
MANASVI ARAVIND RAPAKA <manasviaravind.2@gmail.com>
2025-07-02 22:37:02 +0530

    Add files via upload

---
+++ elif.py
@@ -0,0 +1,11 @@
+#Write a Python program that checks if a number is positive, negative, or zero using if, elif, and
+#else statements.
+
+num=int(input("enter the num"))
+if num>0:
+    print("enter the num is positive")
+elif num<0:
+    print("enter the num is negetive")
+else:
+    print("enter the num is zero")
+    
